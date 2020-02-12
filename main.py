aa4aa0068f601a1f5a7909a6d995fc03ad5f9a53
kimmokal <kimmo.kallonen@helsinki.fi>
2020-02-12 16:11:03 +0200

    Read edgelist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-print("Tämä on kommentti")
+edge_list_path = "../dataset/musae_facebook_edges.csv"
+data_edges = pd.read_csv(edge_list_path)
